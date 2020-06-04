aff537b59947b588fa35f6651943fb1bf6e1fbe6
Pol4bear <pol4bear@gmail.com>
2020-06-05 02:15:27 +0900

    refactor: add function to change require path relatively

--- server/php/deploy.sh
+++ server/php/deploy.sh
@@ -4,7 +4,7 @@ then
   echo "Usage: $0 [Document root] [Sub directory]"
   exit 1
 fi
-
+echo $1
 if [ ! -d "$1" ]
 then
   echo "Document root not exists"
@@ -24,6 +24,18 @@ then
     echo "Sub directory $deploy_path created"
   fi
 fi
+
 cp public/* $deploy_path
 echo "Files in public directory moved to $deploy_path"
+
+if [ "$#" -eq 2 ]
+then
+  relative_path=$(realpath --relative-to=$deploy_path $1/../src)
+  find public -type f -printf "%f\n" | while read file; do
+    sed -i "s~require_once '../src~require_once '$relative_path~g" $deploy_path/$file
+    sed -i "s~require '../src~require '$relative_path~g" $deploy_path/$file
+  done
+  echo "Require paths changed"
+fi
+
 echo "Deploy finished"
