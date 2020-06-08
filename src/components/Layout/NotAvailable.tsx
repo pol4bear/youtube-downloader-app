20d4150a8978bc8b3d717a6cf74b515105911427
Pol4bear <pol4bear@gmail.com>
2020-06-09 00:06:08 +0900

    feat: add service not available page

--- src/components/Layout/NotAvailable.tsx
+++ src/components/Layout/NotAvailable.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { faStickyNote } from '@fortawesome/free-solid-svg-icons';
+import { faSadTear } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useIntl, FormattedMessage } from 'react-intl';
 import CenterAligner from './CenterAligner';
 
-const NotFound: React.FC = () => {
+const NotAvailable: React.FC = () => {
   const intl = useIntl();
 
   return (
     <CenterAligner>
       <Helmet>
-        <title>{intl.messages.pageNotfound}</title>
+        <title>{intl.messages.serviceNotAvailable}</title>
       </Helmet>
       <h1>
-        <FontAwesomeIcon icon={faStickyNote} />{' '}
-        <FormattedMessage id="pageNotfound" />
+        <FontAwesomeIcon icon={faSadTear} />{' '}
+        <FormattedMessage id="serviceNotAvailable" />
       </h1>
     </CenterAligner>
   );
 };
 
-export default NotFound;
+export default NotAvailable;
