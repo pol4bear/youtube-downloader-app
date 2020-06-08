cfcd16882d0af81044ccb6ce822f510f80456ce7
Pol4bear <pol4bear@gmail.com>
2020-06-08 20:40:45 +0900

    feat: add click event

--- src/components/Pages/Search/SearchItemCard.tsx
+++ src/components/Pages/Search/SearchItemCard.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import { Card, Col, Row } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPlay,
+  faThumbsUp,
+  faThumbsDown,
+  faComment,
+} from '@fortawesome/free-solid-svg-icons';
+import { useHistory } from 'react-router';
 import { SearchItem } from './Types';
 
 interface SearchItemCardProps {
@@ -9,16 +17,20 @@ interface SearchItemCardProps {
 const SearchItemCard: React.FC<SearchItemCardProps> = (props) => {
   /* eslint-disable react/no-danger */
   const { data } = props;
+  const history = useHistory();
   return (
     <Card
       style={{ backgroundColor: 'transparent', borderColor: 'transparent' }}
+      onClick={() => {
+        history.push(`../watch/${data.id}`);
+      }}
       hoverable
     >
       <Row gutter={[16, 24]}>
-        <Col span={6}>
+        <Col xs={12} sm={12} md={8} lg={6}>
           <img alt="Thumbnail" src={data.thumbnails.high.url} width="100%" />
         </Col>
-        <Col span={18}>
+        <Col xs={12} sm={12} md={16} lg={18}>
           <Row>
             <Col span={24}>
               <h1 dangerouslySetInnerHTML={{ __html: data.title }} />
@@ -26,33 +38,45 @@ const SearchItemCard: React.FC<SearchItemCardProps> = (props) => {
             <Col span={24}>
               <p dangerouslySetInnerHTML={{ __html: data.description }} />
             </Col>
-            <Col span={6}>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: `${data.statistics.viewCount}`,
-                }}
-              />
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <p>
+                <FontAwesomeIcon icon={faPlay} />{' '}
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: `${data.statistics.viewCount}`,
+                  }}
+                />
+              </p>
             </Col>
-            <Col span={6}>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: `${data.statistics.likeCount}`,
-                }}
-              />
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <p>
+                <FontAwesomeIcon icon={faComment} />{' '}
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: `${data.statistics.commentCount}`,
+                  }}
+                />
+              </p>
             </Col>
-            <Col span={6}>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: `${data.statistics.dislikeCount}`,
-                }}
-              />
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <p>
+                <FontAwesomeIcon icon={faThumbsUp} />{' '}
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: `${data.statistics.likeCount}`,
+                  }}
+                />
+              </p>
             </Col>
-            <Col span={6}>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: `${data.statistics.commentCount}`,
-                }}
-              />
+            <Col xs={12} sm={12} md={6} lg={6}>
+              <p>
+                <FontAwesomeIcon icon={faThumbsDown} />{' '}
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: `${data.statistics.dislikeCount}`,
+                  }}
+                />
+              </p>
             </Col>
           </Row>
         </Col>
